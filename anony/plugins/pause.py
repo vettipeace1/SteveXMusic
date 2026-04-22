116af3d6d9d5cb363a82a70a9b66223f0a93433a
vettipeace1 <yarukitta46@gmail.com>
2026-04-22 15:25:02 +0530

    Update pause.py

--- anony/plugins/pause.py
+++ anony/plugins/pause.py
@@ -2,136 +2,29 @@
 # Licensed under the MIT License.
 # This file is part of AnonXMusic
 
-from pathlib import Path
-
 from pyrogram import filters, types
 
-from anony import anon, app, config, db, lang, queue, tg, yt
-from anony.helpers import buttons, utils
-from anony.helpers._play import checkUB
-from anony.helpers.styled_send import edit_text_styled
+from anony import anon, app, db, lang
+from anony.helpers import buttons, can_manage_vc
+from anony.helpers.styled_send import send_styled
 
 
-def playlist_to_queue(chat_id: int, tracks: list) -> str:
-    text = "<blockquote expandable>"
-    for track in tracks:
-        pos = queue.add(chat_id, track)
-        text += f"<b>{pos}.</b> {track.title}\n"
-    text = text[:1948] + "</blockquote>"
-    return text
-
-
-@app.on_message(
-    filters.command(["play", "playforce", "vplay", "vplayforce"])
-    & filters.group
-    & ~app.bl_users
-)
+@app.on_message(filters.command(["pause"]) & filters.group & ~app.bl_users)
 @lang.language()
-@checkUB
-async def play_hndlr(
-    _,
-    m: types.Message,
-    force: bool = False,
-    m3u8: bool = False,
-    video: bool = False,
-    url: str = None,
-) -> None:
-    sent = await m.reply_text(m.lang["play_searching"])
-    file = None
-    mention = m.from_user.mention
-    media = tg.get_media(m.reply_to_message) if m.reply_to_message else None
-    tracks = []
+@can_manage_vc
+async def _pause(_, m: types.Message):
+    if not await db.get_call(m.chat.id):
+        return await m.reply_text(m.lang["not_playing"])
 
-    if media:
-        setattr(sent, "lang", m.lang)
-        file = await tg.download(m.reply_to_message, sent)
+    if not await db.playing(m.chat.id):
+        return await m.reply_text(m.lang["play_already_paused"])
 
-    elif m3u8:
-        file = await tg.process_m3u8(url, sent.id, video)
+    await anon.pause(m.chat.id)
 
-    elif url:
-        if "playlist" in url:
-            await sent.edit_text(m.lang["playlist_fetch"])
-            tracks = await yt.playlist(
-                config.PLAYLIST_LIMIT, mention, url, video
-            )
-
-            if not tracks:
-                return await sent.edit_text(m.lang["playlist_error"])
-
-            file = tracks[0]
-            tracks.remove(file)
-            file.message_id = sent.id
-        else:
-            file = await yt.search(url, sent.id, video=video)
-
-        if not file:
-            return await sent.edit_text(
-                m.lang["play_not_found"].format(config.SUPPORT_CHAT)
-            )
-
-    elif len(m.command) >= 2:
-        query = " ".join(m.command[1:])
-        file = await yt.search(query, sent.id, video=video)
-        if not file:
-            return await sent.edit_text(
-                m.lang["play_not_found"].format(config.SUPPORT_CHAT)
-            )
-
-    if not file:
-        return await sent.edit_text(m.lang["play_usage"])
-
-    if file.duration_sec > config.DURATION_LIMIT:
-        return await sent.edit_text(
-            m.lang["play_duration_limit"].format(config.DURATION_LIMIT // 60)
-        )
-
-    if await db.is_logger():
-        await utils.play_log(m, sent.link, file.title, file.duration)
-
-    file.user = mention
-    if force:
-        queue.force_add(m.chat.id, file)
-    else:
-        position = queue.add(m.chat.id, file)
-
-        if position != 0 or await db.get_call(m.chat.id):
-            # Use edit_text_styled so "Play Now" shows 🔵 BLUE colour
-            await edit_text_styled(
-                chat_id=m.chat.id,
-                message_id=sent.id,
-                text=m.lang["play_queued"].format(
-                    position,
-                    file.url,
-                    file.title,
-                    file.duration,
-                    m.from_user.mention,
-                ),
-                reply_markup=buttons.play_queued(
-                    m.chat.id, file.id, m.lang["play_now"]
-                ),
-            )
-            if tracks:
-                added = playlist_to_queue(m.chat.id, tracks)
-                await app.send_message(
-                    chat_id=m.chat.id,
-                    text=m.lang["playlist_queued"].format(len(tracks)) + added,
-                )
-            return
-
-    if not file.file_path:
-        fname = f"downloads/{file.id}.{'mp4' if video else 'webm'}"
-        if Path(fname).exists():
-            file.file_path = fname
-        else:
-            await sent.edit_text(m.lang["play_downloading"])
-            file.file_path = await yt.download(file.id, video=video)
-
-    await anon.play_media(chat_id=m.chat.id, message=sent, media=file)
-    if not tracks:
-        return
-    added = playlist_to_queue(m.chat.id, tracks)
-    await app.send_message(
+    # Use send_styled so "Stream paused" button shows 🔴 RED colour
+    await send_styled(
         chat_id=m.chat.id,
-        text=m.lang["playlist_queued"].format(len(tracks)) + added,
+        text=m.lang["play_paused"].format(m.from_user.mention),
+        reply_markup=buttons.controls(m.chat.id),
+        reply_to_message_id=m.id,
     )
