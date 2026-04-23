48825767616875f57c78fb082019bc762035ff53
vettipeace1 <yarukitta46@gmail.com>
2026-04-23 19:39:00 +0530

    Update pause.py

--- anony/plugins/pause.py
+++ anony/plugins/pause.py
@@ -1,12 +1,8 @@
-# Copyright (c) 2025 AnonymousX1025
-# Licensed under the MIT License.
-# This file is part of AnonXMusic
-
-
 from pyrogram import filters, types
 
-from anony import anon, app, db, lang
+from anony import anon, app, db, lang, queue
 from anony.helpers import buttons, can_manage_vc
+from anony.helpers.styled_send import edit_text_styled, edit_caption_styled
 
 
 @app.on_message(filters.command(["pause"]) & filters.group & ~app.bl_users)
@@ -20,7 +16,28 @@ async def _pause(_, m: types.Message):
         return await m.reply_text(m.lang["play_already_paused"])
 
     await anon.pause(m.chat.id)
-    await m.reply_text(
-        text=m.lang["play_paused"].format(m.from_user.mention),
-        reply_markup=buttons.controls(m.chat.id),
-    )
+
+    media = queue.get_current(m.chat.id)
+    if not media or not media.message_id:
+        return
+
+    text = m.lang["play_paused"].format(m.from_user.mention)
+    keyboard = buttons.controls(m.chat.id, status=m.lang["paused"])
+
+    try:
+        if media.video:
+            await edit_caption_styled(
+                chat_id=m.chat.id,
+                message_id=media.message_id,
+                caption=f"{media.caption}\n\n<blockquote>{text}</blockquote>",
+                reply_markup=keyboard,
+            )
+        else:
+            await edit_text_styled(
+                chat_id=m.chat.id,
+                message_id=media.message_id,
+                text=f"{media.caption}\n\n<blockquote>{text}</blockquote>",
+                reply_markup=keyboard,
+            )
+    except Exception:
+        pass
